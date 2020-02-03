11ffd15dd2ce63e6c0da0d0e2bed1d851e5c8f23
luphord <luphord@protonmail.com>
2020-02-03 18:01:09 +0100

    print version number

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -9,8 +9,19 @@ __email__ = '''{{cookiecutter.email}}'''
 __version__ = '''{{cookiecutter.version}}'''
 
 
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='''{{cookiecutter.project_short_description}}''')
+parser.add_argument('--version',
+                    help='Print version number',
+                    default=False,
+                    action='store_true')
+
+
 def main():
-    pass
+    args = parser.parse_args()
+    if args.version:
+        print('''{{cookiecutter.project_name}} ''' + __version__)
 
 
 if __name__ == '__main__':
