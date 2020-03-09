8ab2bba8765dbbd154d34df86d4aa760125efe4a
luphord <luphord@protonmail.com>
2020-03-09 06:59:53 +0100

    add subcommand

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -9,7 +9,7 @@ __email__ = '''{{cookiecutter.email}}'''
 __version__ = '''{{cookiecutter.version}}'''
 
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
 parser = ArgumentParser(description='''{{cookiecutter.project_short_description}}''')
 parser.add_argument('--version',
@@ -17,8 +17,21 @@ parser.add_argument('--version',
                     default=False,
                     action='store_true')
 
+subparsers = parser.add_subparsers(title='subcommands', dest='subcommand',
+                                   help='Available subcommands')
 
-def main():
+mycmd_parser = subparsers.add_parser('mycmd', help='An example subcommand')
+
+
+def _mycmd(args: Namespace) -> None:
+    print('Running mycmd subcommand...')
+    print('mycmd completed')
+
+
+mycmd_parser.set_defaults(func=_mycmd)
+
+
+def main() -> None:
     args = parser.parse_args()
     if args.version:
         print('''{{cookiecutter.project_name}} ''' + __version__)
