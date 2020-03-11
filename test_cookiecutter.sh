00a7130322a144a2f327bbdac2e2f11adb7032d9
luphord <luphord@protonmail.com>
2020-03-11 17:12:49 +0100

    simple "test" script for cookiecutter

---
+++ test_cookiecutter.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo 'executing cookiecutter in temp dir...'
+tmp_dir=$(mktemp -d -t XXXXXXXXXX)
+cookiecutter -o $tmp_dir --replay .
+project_dir=$tmp_dir/$(ls $tmp_dir)
+
+echo
+echo 'starting a repo in newly created project...'
+cd $project_dir
+git init
+git add .
+git commit -m "initial commit"
+
+echo
+echo 'setting up a dev environment...'
+make venv
+. .venv/bin/activate
+
+echo
+echo 'runing linter and tests...'
+make lint test
+
+echo
+echo 'preparing a release...'
+bumpversion minor
+make dist
+
+echo
+echo 'trying a test run of cli...'
+make cli
