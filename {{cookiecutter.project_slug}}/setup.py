ca2b9dbf49fe06b009570eddb43b4db21f350c7b
luphord <luphord@protonmail.com>
2022-04-03 12:59:35 +0200

    update python and dev dependencies

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -36,9 +36,9 @@ setup(
 {%- endif %}
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     description='''{{cookiecutter.project_short_description}}''',
     entry_points={
